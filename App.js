27dee5c09e3bc449e59f54c3e8e16e2078602ac1
LeTranQuocKhanh <20520574@gm.uit.edu.vn>
2022-10-20 15:13:08 +0700

    first-commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, TextInput, View, Button } from 'react-native';
+import { Text, TextInput, View, Button, FlatList } from 'react-native';
 import styled from 'styled-components/native';
 
 const Expression = styled.TextInput`
@@ -8,13 +8,25 @@ const Expression = styled.TextInput`
   border-width: 2px;
 `;
 
+const ResultButton = styled.Button`
+  border-color: red;
+`;
+
+const Item = ({ title }) => (
+  <View>
+    <Text style={{color: 'red'}}>{title}</Text>
+  </View>
+);
+
 const PizzaTranslator = () => {
   const [result, set_result] = useState('');
   const [expression, set_expression] = useState('');
+  const [history, set_history] = useState([])
+  const [search_history, set_search_history] = useState([...history])
 
-  const Calculate_button = styled.Button`
-    
-  `;
+  const renderItem = ({ item }) => (
+    <Item title={item.expression + '=' + item.value} />
+  );
 
   return (
     <View style={{padding: 10}}>
@@ -22,22 +34,54 @@ const PizzaTranslator = () => {
         style={{height: 40}}
         placeholder="Type here to translate!"
         onChangeText={newText => { 
-          // set_expression(newText)
-          try{
-            var a = eval(newText); 
-            set_result(a);
-          } catch (e){
-            set_result("");
-          }
+          set_expression(newText)
         }}
-        // value = {expression}
-        // defaultValue={}
       />
       <Text style={{padding: 10, fontSize: 42}}>
         {
           result
         }
       </Text>
+      <View>
+        <Button title='Calculate' onPress={ () =>{
+          try {
+            var currentResult = eval(expression);
+            set_result(currentResult);
+            var currentHistory = history
+            currentHistory.push({
+              'id': parseInt(history.length) + 1,
+              'expression': expression,
+              'value': currentResult,
+            })
+            set_history(currentHistory)
+            set_search_history(currentHistory)
+            console.log(history)
+            
+
+          }
+          catch(err){
+            console.log(err)
+          }
+        }
+}></Button>
+
+      <Expression placeholder='Type to search'
+      onChangeText={searchString =>{
+        var a = history.filter(value =>{
+          return ( value.expression.includes(
+            searchString) || value.value.toString().includes(searchString)
+          )
+        })
+
+        set_search_history(a)
+      }
+      }/>
+        <FlatList
+        data={search_history}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+      </View>
     </View>
   );
 }
